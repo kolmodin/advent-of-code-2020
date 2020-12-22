4bda1e3418cf0dd8cfa756ebb41d76715deda6c4
Lennart Kolmodin <kolmodin@google.com>
2020-12-22 18:37:25 +0100

    Tidy up day22.

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -8,7 +8,7 @@ fn parse_input(inp: &str) -> Option<(Vec<usize>, Vec<usize>)> {
             player
                 .lines()
                 .skip(1)
-                .map(|ln| ln.parse::<usize>().unwrap())
+                .map(|ln| ln.parse().unwrap())
                 .collect()
         })
         .collect_tuple()
@@ -19,43 +19,43 @@ fn score(deck: &[usize]) -> usize {
         .rev()
         .zip(1..)
         .map(|(a, b)| a * b)
-        .sum::<usize>()
+        .sum()
 }
 
 fn main() {
     let contents = fs::read_to_string("day22.txt").expect("Something went wrong reading the file");
 
     let (player1, player2) = parse_input(&contents).unwrap();
-    let winner_part1 = play(player1, player2, false).get_winner_deck();
+    let winner_part1 = play(player1, player2, false).get_deck();
 
     let (player1, player2) = parse_input(&contents).unwrap();
-    let winner_part2 = play(player1, player2, true).get_winner_deck();
+    let winner_part2 = play(player1, player2, true).get_deck();
 
     println!("Part 1: {}", score(&winner_part1));
     println!("Part 2: {}", score(&winner_part2));
 }
 
-enum Outcome {
-    Player1Win(Vec<usize>),
-    Player2Win(Vec<usize>),
+enum Winner {
+    Player1(Vec<usize>),
+    Player2(Vec<usize>),
 }
 
-impl Outcome {
-    fn get_winner_deck(self) -> Vec<usize> {
+impl Winner {
+    fn get_deck(self) -> Vec<usize> {
         match self {
-            Outcome::Player1Win(deck) => deck,
-            Outcome::Player2Win(deck) => deck,
+            Winner::Player1(deck) => deck,
+            Winner::Player2(deck) => deck,
         }
     }
 }
 
-fn play(mut player1: Vec<usize>, mut player2: Vec<usize>, can_recurse: bool) -> Outcome {
+fn play(mut player1: Vec<usize>, mut player2: Vec<usize>, can_recurse: bool) -> Winner {
     let mut states = HashSet::new();
 
     while !player1.is_empty() && !player2.is_empty() {
         let state: (Vec<usize>, Vec<usize>) = (player1.clone(), player2.clone());
         if states.get(&state).is_some() {
-            return Outcome::Player1Win(player1);
+            return Winner::Player1(player1);
         }
         states.insert(state);
 
@@ -80,11 +80,11 @@ fn play(mut player1: Vec<usize>, mut player2: Vec<usize>, can_recurse: bool) -> 
             player2.iter().take(p2).cloned().collect(),
             can_recurse,
         ) {
-            Outcome::Player1Win(_) => {
+            Winner::Player1(_) => {
                 player1.push(p1);
                 player1.push(p2);
             }
-            Outcome::Player2Win(_) => {
+            Winner::Player2(_) => {
                 player2.push(p2);
                 player2.push(p1);
             }
@@ -92,7 +92,7 @@ fn play(mut player1: Vec<usize>, mut player2: Vec<usize>, can_recurse: bool) -> 
     }
 
     if player1.is_empty() {
-        return Outcome::Player2Win(player2);
+        return Winner::Player2(player2);
     }
-    return Outcome::Player1Win(player1);
+    Winner::Player1(player1)
 }
