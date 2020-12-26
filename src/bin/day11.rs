761132a2fb98abfe9762ac38be260be549fe5261
Lennart Kolmodin <kolmodin@google.com>
2020-12-26 20:56:37 +0100

    Update day11.
    Now actually has the merged day11a/day11b.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,74 +1,53 @@
 #![feature(iter_map_while)]
 
-use std::convert::TryFrom;
+use aoc2020::map::Map;
+use aoc2020::pos2d::Pos;
+
 use std::fs;
 use std::iter::successors;
-use std::str::from_utf8;
 
-type Pos = (i32, i32);
-
-#[derive(PartialEq, Eq)]
-struct Map {
-    vec: Vec<Vec<u8>>,
+trait Rules {
+    fn is_occupied_dir(&self, map: &Map, pos: Pos, dir: Pos) -> bool;
+    fn new_cell(&self, is_occupied: bool, adjacent_occupied: usize) -> Option<u8>;
 }
 
-impl Map {
-    fn new(map: Vec<Vec<u8>>) -> Map {
-        Map { vec: map }
+struct Part1;
+impl Rules for Part1 {
+    fn is_occupied_dir(&self, map: &Map, pos: Pos, dir: Pos) -> bool {
+        map.get(pos + dir) == Some(b'#')
     }
-
-    fn new_with_size(map: &Map, cell: u8) -> Map {
-        Map {
-            vec: map
-                .vec
-                .iter()
-                .map(|row| row.iter().map(|_| cell).collect())
-                .collect(),
+    fn new_cell(&self, is_occupied: bool, adjacent_occupied: usize) -> Option<u8> {
+        match (is_occupied, adjacent_occupied) {
+            (false, 0) => Some(b'#'),
+            (true, n) if n >= 4 => Some(b'L'),
+            _ => None,
         }
-    }
-
-    fn get(&self, pos: (i32, i32)) -> Option<u8> {
-        let (ix, iy) = pos;
-        let x = usize::try_from(ix).ok()?;
-        let y = usize::try_from(iy).ok()?;
-        self.vec.get(y).map(|row| row.get(x)).flatten().cloned()
-    }
-
-    fn set(&mut self, pos: (i32, i32), cell: u8) {
-        let row = self.vec.get_mut(pos.1 as usize).unwrap();
-        row[pos.0 as usize] = cell;
-    }
-
-    fn repeat_delta_from_start(
-        &self,
-        start: (i32, i32),
-        delta: (i32, i32),
-    ) -> impl Iterator<Item = (Pos, u8)> + '_ {
-        successors(Some(start), move |d| Some(step(d, &delta)))
-            .map_while(move |pos| self.get(pos).map(|cell| (pos, cell))).fuse()
-    }
-
-    fn print(&self) {
-        for row in &self.vec {
-            println!("{}", from_utf8(row).unwrap());
-        }
-    }
-
-    fn iter(&self) -> impl Iterator<Item = (Pos, u8)> + '_ {
-        self.iter_pos().zip(self.iter_cells())
-    }
-
-    fn iter_pos(&self) -> impl Iterator<Item = Pos> + '_ {
-        (0..self.vec.len())
-            .flat_map(move |row| (0..self.vec[0].len()).map(move |col| (col as i32, row as i32)))
-    }
-
-    fn iter_cells(&self) -> impl Iterator<Item = u8> + '_ {
-        self.vec.iter().flat_map(|row| row.iter()).cloned()
     }
 }
 
-fn evolve(mmap: &Map) -> Map {
+struct Part2;
+impl Rules for Part2 {
+    fn is_occupied_dir(&self, map: &Map, pos: Pos, dir: Pos) -> bool {
+        map.repeat_delta_from_start(pos, dir)
+            .skip(1)
+            .find_map(|(_, cell)| match cell {
+                b'L' => Some(false),
+
+                b'#' => Some(true),
+                _ => None,
+            })
+            .unwrap_or(false)
+    }
+    fn new_cell(&self, is_occupied: bool, adjacent_occupied: usize) -> Option<u8> {
+        match (is_occupied, adjacent_occupied) {
+            (false, 0) => Some(b'#'),
+            (true, n) if n >= 5 => Some(b'L'),
+            _ => None,
+        }
+    }
+}
+
+fn evolve<R: Rules>(mmap: &Map, rules: &R) -> Map {
     let mut new_mmap = Map::new_with_size(&mmap, b'@');
 
     for (pos, this_cell) in mmap.iter() {
@@ -76,75 +55,45 @@ fn evolve(mmap: &Map) -> Map {
             new_mmap.set(pos, b'.');
             continue;
         }
-        let is_occupied = mmap.get(pos) == Some(b'#');
+        let is_occupied = this_cell == b'#';
 
-        let dirs: Vec<(i32, i32)> = vec![
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, -1),
-            (0, 1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-        ];
-
-        let mut adjacent_occupied = 0;
-
-        for dir in dirs {
-            for (_, cell) in mmap.repeat_delta_from_start(pos, dir).skip(1) {
-                match cell {
-                    b'L' => {
-                        break;
-                    }
-                    b'#' => {
-                        adjacent_occupied += 1;
-                        break;
-                    }
-                    _ => {
-                        // don't break, continue searching.
-                    }
-                }
-            }
-        }
+        let adjacent_occupied = Pos::origo()
+            .neighbors()
+            .into_iter()
+            .filter(|dir| rules.is_occupied_dir(&mmap, pos, *dir))
+            .count();
 
         new_mmap.set(
             pos,
-            match (is_occupied, adjacent_occupied) {
-                (false, 0) => b'#',
-                (true, n) if n >= 5 => b'L',
-                _ => this_cell,
-            },
+            rules
+                .new_cell(is_occupied, adjacent_occupied)
+                .unwrap_or(this_cell),
         );
     }
     new_mmap
 }
 
-fn step(xy: &(i32, i32), dir: &(i32, i32)) -> (i32, i32) {
-    (xy.0 + dir.0, xy.1 + dir.1)
+fn run_evolve_with<R: Rules>(map: Map, rules: R) -> usize {
+    let series = successors(Some(map), |prev| {
+        let next = evolve::<R>(prev, &rules);
+        if prev != &next {
+            Some(next)
+        } else {
+            None
+        }
+    });
+
+    let final_map = series.last().unwrap();
+
+    final_map.iter_cells().filter(|cell| *cell == b'#').count()
 }
 
 fn main() {
-    let contents = fs::read_to_string("inputs/day11.txt").expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string("inputs/day11.txt").expect("Something went wrong reading the file");
 
     let map: Vec<Vec<u8>> = contents.lines().map(|ln| ln.as_bytes().to_vec()).collect();
 
-    let mut series = successors(Some(Map::new(map)), |m| Some(evolve(m)));
-
-    let mut previous = series.next().unwrap();
-    println!("first!");
-    previous.print();
-
-    for next in series {
-        println!("step\n-----");
-        next.print();
-        if previous != next {
-            previous = next;
-            continue;
-        }
-
-        let all = next.iter_cells().filter(|cell| *cell == b'#').count();
-        println!("occupied seats {}", all);
-        break;
-    }
+    println!("Part 1: {}", run_evolve_with(Map::new(&map), Part1 {}));
+    println!("Part 2: {}", run_evolve_with(Map::new(&map), Part2 {}));
 }
