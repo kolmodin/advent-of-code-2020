b2ffeced30b3ec4244e336c1f9b49d3cc50b2de7
Lennart Kolmodin <kolmodin@gmail.com>
2021-01-11 21:29:16 +0100

    Cleanup day08 in rust. Add lost part1.

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,66 +1,86 @@
 use std::fs;
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 enum Instr {
-    NOp,
+    NOp(i32),
     Acc(i32),
     Jmp(i32),
+}
+
+enum Exit {
+    Terminated(i32),
+    Seen(i32),
 }
 
 fn parse_line(ln: &str) -> Instr {
     let parts: Vec<_> = ln.split(' ').collect();
     assert_eq!(parts.len(), 2);
+    let num = parts[1].parse().unwrap();
     match parts[0] {
-        "nop" => Instr::NOp,
-        "acc" => Instr::Acc(parts[1].parse().unwrap()),
-        "jmp" => Instr::Jmp(parts[1].parse().unwrap()),
+        "nop" => Instr::NOp(num),
+        "acc" => Instr::Acc(num),
+        "jmp" => Instr::Jmp(num),
         _ => panic!("unknown instruction {instr}", instr = ln),
     }
+}
+
+fn run(program: &[Instr]) -> Exit {
+    let mut acc = 0;
+    let mut pc = 0;
+    let mut seen = vec![false; program.len()];
+    let term_pc = program.len();
+    while !seen.get(pc).cloned().unwrap_or(false) && pc != term_pc {
+        seen[pc] = true;
+        match program[pc] {
+            Instr::NOp(_) => pc += 1,
+            Instr::Jmp(num) => pc = (pc as i32 + num) as usize,
+            Instr::Acc(num) => { 
+                acc += num; 
+                pc += 1;
+            },
+        }
+    }
+    if pc == term_pc {
+        Exit::Terminated(acc)
+    } else{
+        Exit::Seen(acc)
+    }
+}
+
+fn part1(program: &[Instr]) -> i32 {
+    if let Exit::Seen(num) = run(program) {
+        num
+    } else {
+        panic!("unexpected exit code in part1");
+    }
+}
+
+
+fn part2(program: &mut [Instr]) -> i32 {
+    for i in 0..program.len() {
+        let op = program[i];
+        let new_op = match op {
+            Instr::NOp(num) => Some(Instr::Jmp(num)),
+            Instr::Jmp(num) => Some(Instr::NOp(num)),
+            _ => None,
+        };
+        if !new_op.is_some() {
+            continue;
+        }
+        program[i] = new_op.unwrap();
+        if let Exit::Terminated(acc) = run(program) {
+            return acc;
+        }
+        program[i] = op;
+    }
+    panic!("part2: couldn't make program terminate.")
 }
 
 fn main() {
     let contents = fs::read_to_string("inputs/day08.txt").expect("Something went wrong reading the file");
 
-    let instrs: Vec<Instr> = contents.lines().map(|ln| parse_line(ln)).collect();
+    let mut program: Vec<Instr> = contents.lines().map(parse_line).collect();
 
-    for i in 0..instrs.len() {
-        let mut my_instrs = instrs.clone();
-        if let Instr::Jmp(_) = my_instrs[i] {
-            my_instrs[i] = Instr::NOp;
-        } else {
-            continue;
-        }
-
-        let mut visited: Vec<bool> = my_instrs.iter().map(|_| false).collect();
-
-        let mut acc: i32 = 0;
-        let mut pc: i32 = 0;
-        let mut pc_before_jmp: Vec<i32> = Vec::new();
-        loop {
-            if visited[pc as usize] {
-                println!("second time running pc={}, acc={}", pc, acc);
-                break;
-            }
-            if pc >= (my_instrs.len() as i32) - 1 {
-                println!("reached end of program, pc={}, acc={}", pc, acc);
-                return;
-            }
-            visited[pc as usize] = true;
-            let instr = my_instrs.get(pc as usize).unwrap();
-            match instr {
-                Instr::NOp => {
-                    pc += 1;
-                }
-                Instr::Acc(arg) => {
-                    acc += arg;
-                    pc += 1;
-                }
-                Instr::Jmp(arg) => {
-                    pc_before_jmp.push(pc);
-                    pc += arg;
-                }
-            }
-        }
-    }
-    println!("terminated");
+    println!("Part 1: {}", part1(&program));
+    println!("Part 2: {}", part2(&mut program));
 }
